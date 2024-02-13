83bfac6fd4d3056263dd4fdd7db727f43174dea7
mostafa <mostafarasekh5@gamil.com>
2024-02-13 13:58:36 -0800

    update islami

---
+++ lib/cubits/states.dart
@@ -0,0 +1,5 @@
+abstract class StatesApp{}
+class EnglishInitialLanguage extends StatesApp{}
+class ArabicLanguage extends StatesApp{}
+class DarkState extends StatesApp{}
+class LightState extends StatesApp{}
